9c7736250a2ef053864463d98d03c1bf08a90dac
BiggerHao <chenghao.buaa@gmail.com>
2021-02-17 16:38:14 +0800

    feat: update statechart to handle DOM events

--- tooltip/state-chart.js
+++ tooltip/state-chart.js
@@ -1,40 +1,81 @@
-const { createMachine, interpret } = XState;
+const { createMachine, assign, interpret } = XState;
 
-const tooltipMachine = createMachine({
-  id: "tooltip",
-  initial: "initialized",
-  context: {},
-  states: {
-    initialized: {
-      on: {
-        ENTER_WIDGET: "onWidget",
-        LEAVE_WIDGET: "outsideWidget",
-      },
+const button = document.getElementById("button");
+const tooltip = document.getElementById("tooltip");
+
+const tooltipMachine = createMachine(
+  {
+    id: "tooltip",
+    initial: "outsideWidget",
+    context: {
+      clientX: 0,
+      clientY: 0,
     },
-    onWidget: {
-      initial: "tooltipInvisible",
-      on: {
-        LEAVE_WIDGET: "outsideWidget",
-      },
-      states: {
-        tooltipInvisible: {
-          on: {
-            MOUSE_MOVE: "tooltipInvisible",
-            STOP_MOVING: "tooltipVisible",
+    states: {
+      onWidget: {
+        initial: "tooltipInvisible",
+        on: {
+          mouseleave: "outsideWidget",
+        },
+        states: {
+          tooltipInvisible: {
+            on: {
+              mousemove: {
+                target: "tooltipInvisible",
+                actions: assign({
+                  clientX: (context, event) => event.clientX,
+                  clientY: (context, event) => event.clientY,
+                }),
+              },
+            },
+            after: {
+              200: "tooltipVisible",
+            },
+          },
+          tooltipVisible: {
+            entry: "showTooltip",
           },
         },
-        tooltipVisible: {},
       },
-    },
-    outsideWidget: {
-      on: {
-        ENTER_WIDGET: "onWidget",
+      outsideWidget: {
+        entry: "hideTooltip",
+        on: {
+          mouseenter: "onWidget",
+        },
       },
     },
   },
-});
+  {
+    actions: {
+      showTooltip: (context, event) => {
+        tooltip.style.visibility = "visible";
+        tooltip.style.top = context.clientY + "px";
+        tooltip.style.left = context.clientX + "px";
+      },
+      hideTooltip: (context, event) => {
+        if (event.type != "xstate.init") {
+          tooltip.style.visibility = "hidden";
+        }
+      },
+    },
+  }
+);
 
 const tooltipService = interpret(tooltipMachine).onTransition((state) => {
   const states = state.toStrings();
   console.log(`\t${states[states.length - 1]}\t\t\t${state.event.type}`);
 });
+
+button.addEventListener("mouseenter", (event) => {
+  tooltipService.send(event);
+});
+
+button.addEventListener("mouseleave", (event) => {
+  tooltipService.send(event);
+});
+
+button.addEventListener("mousemove", (event) => {
+  tooltipService.send(event);
+});
+
+tooltipService.start();
